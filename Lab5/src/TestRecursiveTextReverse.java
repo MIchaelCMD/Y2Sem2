3a5cb8ed64e4b5efa625cf1de40fb5741f57710c
MichaelCMD <43179251+MIchaelCMD@users.noreply.github.com>
2019-02-14 18:49:57 +0000

    Changed the TestRecursiveTextReverse class

--- Lab5/src/TestRecursiveTextReverse.java
+++ Lab5/src/TestRecursiveTextReverse.java
@@ -1,9 +1,15 @@
+import javax.swing.*;
+
 public class TestRecursiveTextReverse {
 
     public static void main(String[] args) {
 
         String reverseResult = RecursiveTextReverse.reverse("R");
         System.out.println(reverseResult);
+
+
+        String reverseOutput = RecursiveTextReverse.reverse(JOptionPane.showInputDialog("Enter a string to be reversed"));
+        JOptionPane.showMessageDialog(null,reverseOutput,"Reversed Message",JOptionPane.INFORMATION_MESSAGE);
 
     }
 
